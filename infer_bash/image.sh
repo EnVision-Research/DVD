75c4c50581d7991fe75f23f3d8857257e3a6cfe1
Hongfei (Faye) Zhang <hongfei@u.nus.edu>
2026-03-25 19:50:08 +0800

    Update image.sh

--- infer_bash/image.sh
+++ infer_bash/image.sh
@@ -1,4 +1,4 @@
 CKPT='ckpt'
-IMAGE_BASE_DATA_DIR='/hpc2hdd/JH_DATA/share/jhe812/PrivateShareGroup/jhe812_d4p_datasets/d4p_datasets/eval/depth'
+IMAGE_BASE_DATA_DIR='your_image_root'
 
 python test_script/test_from_trained_all_img.py --ckpt $CKPT --base_data_dir $IMAGE_BASE_DATA_DIR
